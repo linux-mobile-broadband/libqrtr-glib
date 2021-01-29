4903ebbcd34e0091673fa13a00db830aa66da24f
Aleksander Morgado <aleksander@aleksander.es>
2021-01-29 11:13:10 +0100

    build: setup autotools based project

---
+++ autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+REQUIRED_AUTOMAKE_VERSION=1.9
+PKG_NAME=libqrtr
+
+(test -f $srcdir/configure.ac \
+  && test -f $srcdir/src/libqrtr-glib/libqrtr-glib.h) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
+}
+
+(cd $srcdir;
+    autoreconf --force --install --verbose
+)
+
+if test -z "$NOCONFIGURE"; then
+    $srcdir/configure --enable-maintainer-mode "$@"
+fi
